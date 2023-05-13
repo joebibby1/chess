0037c4a69c792f06866d8c3d4593ee423ced10c0
joebibby1 <joe.bibby@cambridgekinetics.com>
2023-05-13 13:15:18 +0100

    color the squares and pieces correctly

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,8 +1,7 @@
 <template>
-  <div>test</div>
   <div v-for="(row, rowIndex) in board.pieces" class="board-row">
     <div v-for="(column, columIndex) in board.pieces[rowIndex]">
-      <Piece
+      <board-square
         :piece="board.pieces[rowIndex][columIndex]"
         :position="[rowIndex, columIndex]"
       />
@@ -12,26 +11,14 @@
 
 <script setup lang="ts">
 import { Board } from "../models/board";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faChessQueen } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import Piece from "./Piece.vue";
-
-library.add(faChessQueen);
+import BoardSquare from "./BoardSquare.vue";
 
 const board: Board = new Board();
 </script>
-
 <style scoped>
 .board-row {
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  height: 40px;
-  width: 320px;
-  background: linear-gradient(90deg, #eeeeee 50%, #aaaaaa 50%);
-  background-size: 80px 80px;
 }
 
 .black-piece {
---
+++ src/components/BoardSquare.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="`${sqaureBackground} board-square`">
+    <div v-if="props.piece" :class="pieceColor">
+      <font-awesome-icon :icon="props.piece?.icon" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { Piece } from "../models/pieces";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChessQueen,
+  faChessRook,
+  faChessKing,
+  faChessPawn,
+  faChessKnight,
+  faChessBishop,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faChessQueen,
+  faChessRook,
+  faChessKing,
+  faChessPawn,
+  faChessKnight,
+  faChessBishop
+);
+
+const props = defineProps({
+  piece: {
+    type: Object as () => Piece | null,
+    required: true,
+  },
+  position: {
+    type: Array as () => number[],
+    required: true,
+  },
+});
+
+// squares with a sum index that is even are white
+const sqaureBackground =
+  (props.position[0] + props.position[1]) % 2 === 0
+    ? "background-white"
+    : "background-black";
+
+const pieceColor = props.piece?.isWhite ? "piece-white" : "piece-black";
+</script>
+
+<style scoped>
+.background-white {
+  background-color: #eeeeee;
+}
+
+.background-black {
+  background-color: #aaaaaa;
+}
+
+.piece-white {
+  color: white;
+}
+
+.piece-black {
+  color: black;
+}
+
+.board-square {
+  /* display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center; */
+  height: 40px;
+  width: 40px;
+}
+</style>
